e39a0a80e39f940a8a82c5c037e649e6b747e6e2
Danieljoha <djo060@uit.no>
2023-01-23 12:13:15 +0100

    fun=dritt

--- A_Small_task.R
+++ A_Small_task.R
@@ -1,17 +1,23 @@
 library(tidyverse)
 library(readr)
 library(lubridate)
-test <- read_table("http://vortex.nsstc.uah.edu/data/msu/v6.0/tlt/uahncdc_lt_6.0.txt")
-test <- sapply(test, as.numeric)
-test <- as_tibble(test)
+Temp_data <- read_table("http://vortex.nsstc.uah.edu/data/msu/v6.0/tlt/uahncdc_lt_6.0.txt")
+temp <- sapply(Temp_data, as.numeric)
+temp <- as_tibble(temp)
 
 
-test1 <- test %>% 
+temp <- temp[-(530:541),] #får bort skiten på bunnen
+
+temp <- temp %>% 
   mutate(date = ym(paste(Year, Mo)))
-test1 <- test1 %>% 
+temp <- temp %>% 
   select(date, everything())
 
 
-test1 %>% 
+temp %>% 
   ggplot(aes(x=date, y=Globe))+
   geom_line()
+
+temp1 <- temp %>% 
+  aggregate(Globe~Year, FUN=mean)
+
